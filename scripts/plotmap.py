59ac3f4b4c12daf77328377327119288102deb38
Gregory Green <gregorymgreen@gmail.com>
2013-03-26 16:27:30 -0400

    plotmap.py can now use piecewise-linear model

--- scripts/plotmap.py
+++ scripts/plotmap.py
@@ -33,8 +33,9 @@ import argparse, sys
 
 import healpy as hp
 import h5py
+		
 
-def getClouds(fname):
+def getLOS(fname):
 	f = h5py.File(fname, 'r')
 	
 	# Get a list of pixels in the file
@@ -47,67 +48,165 @@ def getClouds(fname):
 	nPixels = len(pixels)
 	
 	# Get number of clouds and samples
-	tmp, nSamples, nClouds = f['pixel %d/clouds' % pixels[0]].shape
-	nClouds = (nClouds - 1) / 2
+	nCloudSamples, nClouds = 1, 1
+	try:
+		tmp, nCloudSamples, nClouds = f['pixel %d/clouds' % pixels[0]].shape
+		nClouds = (nClouds - 1) / 2
+	except:
+		pass
+	shape = (nPixels, nCloudSamples, nClouds)
+	CloudDeltaMu = np.zeros(shape, dtype='f8')
+	CloudDeltaLnEBV = np.zeros(shape, dtype='f8')
 	
-	# Extract cloud fit from each pixel
-	shape = (nPixels, nSamples, nClouds)
-	DeltaMu = np.zeros(shape, dtype='f8')
-	DeltaLnEBV = np.zeros(shape, dtype='f8')
+	# Get number of regions and samples for piecewise model
+	nPiecewiseSamples, nSlices = 1, 1
+	try:
+		tmp, nPiecewiseSamples, nSlices = f['pixel %d/los' % pixels[0]].shape
+		nSlices -= 1
+	except:
+		pass
+	shape = (nPixels, nPiecewiseSamples, nSlices)
+	PiecewiseDeltaLnEBV = np.zeros(shape, dtype='f8')
+	
+	# Extract l.o.s. fits from each pixel
 	for i,pixIdx in enumerate(pixels):
+		# Cloud model
 		try:
 			group = f['pixel %d/clouds' % pixIdx]
 		except:
-			continue
+			pass
 		
 		try:
-			DeltaMu[i] = group[0, :, 1:nClouds+1]
-			DeltaLnEBV[i] = group[0, :, nClouds+1:]
+			CloudDeltaMu[i] = group[0, :, 1:nClouds+1]
+			CloudDeltaLnEBV[i] = group[0, :, nClouds+1:]
 		except:
-			DeltaMu[i] = 0
-			DeltaLnEBV[i] = np.nan
+			CloudDeltaMu[i] = 0
+			CloudDeltaLnEBV[i] = np.nan
+		
+		# Piecewise model
+		try:
+			group = f['pixel %d/los' % pixIdx]
+		except:
+			pass
+		
+		try:
+			PiecewiseDeltaLnEBV[i] = group[0,:,1:]
+		except:
+			PiecewiseDeltaLnEBV[i] = np.nan
 	
-	muAnchor = np.cumsum(DeltaMu, axis=2)
-	DeltaEBV = np.exp(DeltaLnEBV)
+	CloudMuAnchor = np.cumsum(CloudDeltaMu, axis=2)
+	CloudDeltaEBV = np.exp(CloudDeltaLnEBV)
+	PiecewiseDeltaEBV = np.exp(PiecewiseDeltaLnEBV)
 	
-	return pixels, muAnchor, DeltaEBV
+	return pixels, CloudMuAnchor, CloudDeltaEBV, PiecewiseDeltaEBV
 
-def getCloudsFromMultiple(fnames):
+def getLOSFromMultiple(fnames):
 	partial = []
 	nPixels = 0
 	for fname in fnames:
 		try:
-			partial.append( getClouds(fname) )
-			nPixels += len(partial[-1][1])
+			partial.append(getLOS(fname))
+			nPixels += len(partial[-1][0])
 		except:
 			pass
-	tmp, nSamples, nClouds = partial[0][1].shape
-	
 	pixels = np.empty(nPixels, dtype='u4')
-	shape = (nPixels, nSamples, nClouds)
-	muAnchor = np.empty(shape, dtype='f8')
-	DeltaEBV = np.empty(shape, dtype='f8')
+	
+	tmp, nCloudSamples, nClouds = partial[0][1].shape
+	shape = (nPixels, nCloudSamples, nClouds)
+	CloudMuAnchor = np.empty(shape, dtype='f8')
+	CloudDeltaEBV = np.empty(shape, dtype='f8')
+	
+	tmp, nPiecewiseSamples, nSlices = partial[0][3].shape
+	shape = (nPixels, nPiecewiseSamples, nSlices)
+	PiecewiseDeltaEBV = np.empty(shape, dtype='f8')
 	
 	startIdx = 0
 	for i,part in enumerate(partial):
 		endIdx = startIdx + len(part[1])
 		pixels[startIdx:endIdx] = part[0][:]
-		muAnchor[startIdx:endIdx] = part[1][:]
-		DeltaEBV[startIdx:endIdx] = part[2][:]
+		CloudMuAnchor[startIdx:endIdx] = part[1][:]
+		CloudDeltaEBV[startIdx:endIdx] = part[2][:]
+		PiecewiseDeltaEBV[startIdx:endIdx] = part[3][:]
 		startIdx += len(part[1])
 	
-	return pixels, muAnchor, DeltaEBV
+	return pixels, CloudMuAnchor, CloudDeltaEBV, PiecewiseDeltaEBV
 
-def calcEBV(muAnchor, DeltaEBV, mu):
+def calcCloudEBV(muAnchor, DeltaEBV, mu):
 	foreground = (muAnchor < mu)
 	return np.cumsum(foreground * DeltaEBV, axis=2)[:,:,-1]
+
+def calcPiecewiseEBV(muAnchor, DeltaEBV, mu):
+	nPixels, nSamples, nSlices = DeltaEBV.shape
+	
+	idx = np.where(muAnchor >= mu, np.arange(nSlices), nSlices+1)
+	lowIdx = np.min(idx)
+	
+	EBVslice = np.cumsum(DeltaEBV, axis=2)
+	
+	if lowIdx == nSlices - 1:
+		return EBVslice[:,:,-1]
+	
+	lowMu = muAnchor[lowIdx]
+	highMu = muAnchor[lowIdx+1]
+	
+	a = (mu - lowMu) / (highMu - lowMu)
+	EBVinterp = (1. - a) * EBVslice[:,:,lowIdx]
+	EBVinterp += a * EBVslice[:,:,lowIdx+1]
+	
+	return EBVinterp
+
+def calcPiecewiseEBV2(muAnchor, DeltaEBV, mu):
+	'''
+	Evaluates E(B-V) at a set of distances for the piecewise-linear model.
+	
+	Inputs:
+	    muAnchor  (nSlices)                     Distances of slices returned by bayestar.
+	    DeltaEBV  (nPixels, nSamples, nSlices)  DeltaEBV between slices.
+	    mu        (nMu)                         Distances at which to evaluate E(B-V).
+	
+	Outputs:
+	'''
+	
+	nPixels, nSamples, nSlices = DeltaEBV.shape
+	nMu = mu.shape
+	
+	print type(mu)
+	print mu.dtype
+	print mu.shape
+	print nMu
+	
+	muTmp = mu.copy()
+	muAnchorTmp = muAnchor.copy()
+	muTmp.shape = (nMu, 1)
+	muAnchorTmp.shape = (1, nSlices)
+	muTmp = np.repeat(muTmp, nSlices, axis=1)
+	muAnchorTmp = np.repeat(muAnchorTmp, nMu, axis=0)
+	
+	muDiff = muAnchorTmp - muTmp
+	n = np.arange(nSlices)
+	n.shape = (1, nSlices)
+	n = np.repeat(n, nMu, axis=0)
+	
+	idx = np.where(muDiff >= 0., n, nSlices+1)
+	lowIdx = np.min(idx, axis=1)
+	
+	lowMu = muAnchor[lowIdx]
+	highMu = muAnchor[lowIdx+1]	# TODO: deal with boundary cases
+	
+	EBVinterp = np.zeros(DeltaEBV.shape, dtype='f8')
+	EBVslice = np.cumsum(DeltaEBV, axis=2)
+	
+	a = (mu - lowMu) / (highMu - lowMu)
+	for i in xrange(nMu):
+		EBVinterp[:,:,i] += (1. - a[i]) * EBVslice[:,:,lowIdx[i]]
+		EBVinterp[:,:,i] += a[i] * EBVslice[:,:,highIdx[i]]
+	
+	return EBVinterp
+	
 
 def rasterizeMap(pixels, EBV, nside=512, nest=True, oversample=4):
 	# Determine pixel centers and bounds
 	pixels = np.array(pixels)
-	#print type(nside), nside
-	#print pixels.dtype, pixels
-	#print type(nest), nest
 	theta, phi = hp.pix2ang(nside, pixels, nest=nest)
 	lCenter, bCenter = 180./np.pi * phi, 90. - 180./np.pi * theta
 	
@@ -150,15 +249,16 @@ def rasterizeMap(pixels, EBV, nside=512, nest=True, oversample=4):
 	bounds = (lMin, lMax, bMin, bMax)
 	return img, bounds
 
-def plotEBV(ax, pixels, muAnchor, DeltaEBV, mu, nside=512, nest=True, **kwargs):
+def plotEBV(ax, pixels, muAnchor, DeltaEBV, mu,
+                nside=512, nest=True, model='piecewise', **kwargs):
 	# Generate rasterized image of E(B-V)
-	EBV = calcEBV(muAnchor, DeltaEBV, mu)
-	#idx1 = np.arange(EBV.shape[0])
-	#idx2 = np.random.randint(EBV.shape[1], size=EBV.shape[1])
-	#print EBV.shape
-	#print np.median(EBV, axis=1).shape
-	#EBV = EBV[idx1,idx2]
-	#print EBV.shape
+	EBV = None
+	if model == 'piecewise':
+		EBV = calcPiecewiseEBV(muAnchor, DeltaEBV, mu)
+	elif model == 'clouds':
+		EBV = calcCloudEBV(muAnchor, DeltaEBV, mu)
+	else:
+		raise ValueError("Unrecognized extinction model: '%s'" % model)
 	EBV = np.median(EBV, axis=1) #np.percentile(EBV, 95., axis=1) - np.percentile(EBV, 5., axis=1) #np.mean(EBV, axis=1)
 	img, bounds = rasterizeMap(pixels, EBV, nside, nest)
 	
@@ -192,6 +292,9 @@ def main():
 	                                     help='DM min, DM max, # of distance slices.')
 	parser.add_argument('--nside', '-n', type=int, default=512,
 	                                     help='HealPIX nside parameter.')
+	parser.add_argument('--model', '-m', type=str, default='piecewise',
+	                                     choices=('piecewise', 'clouds'),
+	                                     help="Extinction model: 'piecewise' or 'clouds'")
 	if 'python' in sys.argv[0]:
 		offset = 2
 	else:
@@ -200,7 +303,10 @@ def main():
 	
 	fnames = args.input
 	#fnames = ['/n/wise/ggreen/bayestar/output/CMa.%.5d.h5' % i for i in xrange(18)]
-	pixels, muAnchor, DeltaEBV = getCloudsFromMultiple(fnames)
+	pixels, CloudMuAnchor, CloudDeltaEBV, PiecewiseDeltaEBV = getLOSFromMultiple(fnames)
+	
+	tmp1, tmp2, nSlices = PiecewiseDeltaEBV.shape
+	PiecewiseMuAnchor = np.linspace(5., 20., nSlices)
 	
 	'''
 	nMu = 100
@@ -234,24 +340,35 @@ def main():
 	muN = int(args.dists[2])
 	mu = np.linspace(muMin, muMax, muN)
 	
-	EBVs = calcEBV(muAnchor, DeltaEBV, mu[-1])
+	# Get upper limit on E(B-V)
+	EBVs = None
+	if args.model == 'piecewise':
+		EBVs = calcPiecewiseEBV(PiecewiseMuAnchor, PiecewiseDeltaEBV, mu[-1])
+	elif args.model == 'clouds':
+		EBVs = calcCloudEBV(CloudMuAnchor, CloudDeltaEBV, mu[-1])
 	EBVmax = np.percentile(EBVs, 98.)
 	del EBVs
 	
+	# Determine output filename
 	fname = args.output
 	if fname != None:
 		if fname.endswith('.png'):
 			fname = fname[:-4]
 	
+	# Plot at each distance
 	for i in xrange(muN):
 		print 'mu = %.2f (%d of %d)' % (mu[i], i+1, muN)
 		
 		fig = plt.figure(dpi=150)
 		#grid = ImageGrid(fig, 111, nrows_ncols=(3, 3), axes_pad=0.05)
 		ax = fig.add_subplot(1,1,1)
-		plotEBV(ax, pixels, muAnchor, DeltaEBV, mu[i],
-		        nside=args.nside, nest=True, vmin=0., vmax=EBVmax)
-	
+		if args.model == 'piecewise':
+			plotEBV(ax, pixels, PiecewiseMuAnchor, PiecewiseDeltaEBV, mu[i],
+			        nside=args.nside, nest=True, model=args.model, vmin=0., vmax=EBVmax)
+		elif args.model == 'clouds':
+			plotEBV(ax, pixels, CloudMuAnchor, CloudDeltaEBV, mu[i],
+			        nside=args.nside, nest=True, model=args.model, vmin=0., vmax=EBVmax)
+		
 		ax.set_xlabel(r'$\ell$', fontsize=16)
 		ax.set_ylabel(r'$b$', fontsize=16)
 		
@@ -260,8 +377,11 @@ def main():
 		ax.yaxis.set_major_locator(MaxNLocator(nbins=4))
 		ax.yaxis.set_minor_locator(AutoMinorLocator())
 		
+		d = 10.**(mu[i]/5. - 2.)
+		ax.set_title(r'$\mu = %.2f \ \ \ d = %.2f \, \mathrm{kpc}$' % (mu[i], d), fontsize=16)
+		
 		if fname != None:
-			full_fname = '%s.%.2d.png' % (fname, i)
+			full_fname = '%s.%s.%.2d.png' % (fname, args.model, i)
 			fig.savefig(full_fname, dpi=150)
 	
 	if args.show:
