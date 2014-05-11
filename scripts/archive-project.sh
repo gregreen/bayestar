92aad8d71b66d6f2fcc3baf2df62bfd532ac6b1a
Gregory Green <gregorymgreen@gmail.com>
2014-05-10 21:38:46 -0400

    Allow deployment of Bayestar as tarball.

---
+++ scripts/archive-project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export COMMIT_HASH=`git rev-parse HEAD`
+git-archive --format=tar --output=$1 ${COMMIT_HASH}
+echo ${COMMIT_HASH} > commit.txt
+tar --append --file=$1 commit.txt
+gzip -9 $1
+
+echo "Project archived to $1.gz"
