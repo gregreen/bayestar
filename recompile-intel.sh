50fd74e1be4113d3fe15ec968c3e6862d6ea2503
Gregory Green <gregorymgreen@gmail.com>
2015-12-09 18:28:20 -0500

    Intel compile flags set properly.

--- recompile-intel.sh
+++ recompile-intel.sh
@@ -4,5 +4,6 @@
 rm cmake_install.cmake
 rm CMakeCache.txt
 rm -R CMakeFiles/
+#cmake . -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc
 cmake . -DCMAKE_CXX_COMPILER=icpc -DCMAKE_C_COMPILER=icc
 make -j
