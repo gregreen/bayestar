875ea2508aaccdbbb6b86d8c0c541935bb6cea41
Gregory Green <gregorymgreen@gmail.com>
2017-12-22 17:10:52 -0500

    Plot PCA decomposition of MCMC chains.

---
+++ scripts/chain_convergence.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division
+
+import numpy as np
+import h5py
+import matplotlib
+import matplotlib.pyplot as plt
+
+from argparse import ArgumentParser
+
+
+def load_chain(fname, dset):
+    with h5py.File(fname, 'r') as f:
+        # dset.shape = (chain, GR+best+sample, lnp+parameter)
+        dset = f[dset][0,2:-1,1:]
+    return dset
+
+
+def PCA(data):
+    # Calculate the eigendecomposition of the covariance matrix
+    C = np.cov(data, rowvar=False)
+    eival, eivec = np.linalg.eigh(C)
+    
+    # Sort the eigenvalues/eigenvectors (largest to smallest)
+    idx = np.argsort(eival)[::-1]
+    eival = eival[idx]
+    eivec = eivec[:,idx]
+    eivec = eivec[:,:] / np.linalg.norm(eivec, axis=1)[:,None]
+    
+    # Transform the data to the new coordinate system
+    d_transf = np.dot(data - np.mean(data, axis=0), eivec)
+
+    # Returns the (eigenvalues, eigenvectors, transormed data)
+    return eival, eivec, d_transf
+
+
+def autocorr(data):
+    acorr = []
+    for dim in data.T:
+        acorr.append(np.correlate(dim, dim, mode='same'))
+    return np.vstack(acorr)
+
+
+def main():
+    parser = ArgumentParser(
+        description='Analyze convergence of MCMC chain.',
+        add_help=True)
+    parser.add_argument(
+        '-i', '--input',
+        metavar='file.h5',
+        type=str,
+        required=True,
+        help='Input filename')
+    parser.add_argument(
+        '-d', '--dataset',
+        metavar='/path/to/dataset',
+        type=str,
+        required=True,
+        help='Location of chain dataset inside file.')
+    parser.add_argument(
+        '-o', '--output',
+        metavar='plot.png',
+        type=str,
+        required=True,
+        help='Filename to save figure to.')
+    args = parser.parse_args()
+
+    chain = load_chain(args.input, args.dataset)
+    eival, eivec, chain_transf = PCA(chain)
+    #print(eival)
+    
+    #acorr = autocorr(chain_transf[:,:1] - np.mean(chain_transf[:,:1], axis=0))
+
+    fig = plt.figure(figsize=(8,10), dpi=200)
+    
+    # Principal component coefficients vs. time
+    ax = fig.add_subplot(3,1,1)
+    
+    x = np.arange(chain_transf.shape[0])
+    
+    alpha = np.abs(eival)
+    alpha /= np.max(alpha)
+
+    for k in range(10):
+        y = np.sqrt(eival[k]) * chain_transf[:,k]
+        ax.plot(x, y, c='b', alpha=alpha[k])
+
+    ax.axhline(y=0., c='k', alpha=0.25)
+    
+    ax.set_xlabel(r'$\mathrm{sample\ \#}$', fontsize=10)
+    ax.set_ylabel(r'$\mathrm{principal\ component\ strength}$', fontsize=10)
+    
+    # Principal components
+    ax = fig.add_subplot(3,1,2)
+    
+    x = np.arange(eivec.shape[1])
+    for k in range(10):
+        y = np.sqrt(eival[k]) * eivec[:,k]
+        ax.plot(x, y, c='b', alpha=alpha[k])
+    
+    ax.set_xlabel(r'$\mathrm{distance\ bin}$', fontsize=10)
+    ax.set_ylabel(r'$\mathrm{principal\ component}$', fontsize=10)
+
+    # Raw data
+    ax = fig.add_subplot(3,1,3)
+    x = np.arange(chain.shape[1])
+    y0 = np.mean(chain, axis=0)
+    for k in range(chain.shape[0]):
+        y = chain[k] - y0
+        ax.plot(x, y, c='b', alpha=0.1)
+    
+    ax.set_xlabel(r'$\mathrm{distance\ bin}$', fontsize=10)
+    ax.set_ylabel(r'$\mathrm{raw\ data\ \left( mean\ subtracted \right)}$', fontsize=10)
+    
+    # Save figure
+    fig.savefig(args.output, bbox_inches='tight', dpi=200)
+    
+    return 0
+
+
+if __name__ == '__main__':
+    main()
--- scripts/plot_test_suite.sh
+++ scripts/plot_test_suite.sh
@@ -3,9 +3,11 @@
 #cloudPattern="(?<=cl)([0-9]+\.?[0-9]*\-?)*(?=\.h5)"
 cloudPattern="cl([0-9]+\.?[0-9]*\-?)*_n"
 
-plotDir="/n/fink2/www/ggreen/mock-tests/test-suite/plots/"
+gitHash=$(git rev-parse --short HEAD)
 
-for f in test-suite/output/*.h5; do
+plotDir="/n/fink2/www/ggreen/mock-tests/test-suite/plots/${gitHash}"
+
+for f in test-suite/output/${gitHash}/*.h5; do
     if [[ $f =~ $cloudPattern ]]; then
         cloudSpec=${BASH_REMATCH#cl}
         cloudSpec=${cloudSpec%_n}
@@ -13,19 +15,27 @@ for f in test-suite/output/*.h5; do
         echo ${cloudSpec}
     fi
     baseFname="${f/output/plots}"
-    catFname="${f/output/input}"
+    baseFname="${baseFname%.h5}"
+    catFname="${f/output\/${gitHash}/input}"
     
-    outFname="${baseFname%.h5}_sightline.png"
+    outFname="${baseFname}_sightline.png"
     if [ ! -f "${outFname}" ]; then
         echo "$f -> $outFname"
         python scripts/plotpdf.py "$f" 512 1 -o "${outFname}" -pdfs -dsc -ovplt $cloudSpec -y 2.0 -cat "${catFname}"
         cp "${outFname}" "${plotDir}"
     fi
     
-    outFname="${baseFname%.h5}_lineint.png"
+    outFname="${baseFname}_lineint.png"
     if [ ! -f "${outFname}" ]; then
         echo "$f -> $outFname"
         python scripts/calculate_line_integrals.py -i "$f" -l 512 1 -o "${outFname}" -cl ${cloudSpec} -cat "${catFname}"
+        cp "${outfname}" "${plotDir}"
+    fi
+    
+    outFname="${baseFname}_pca.png"
+    if [ ! -f "${outFname}" ]; then
+        echo "$f -> $outFname"
+        python scripts/chain_convergence.py -i "$f" -d "pixel 512-1/discrete-los" -o "${outFname}" 
         cp "${outFname}" "${plotDir}"
     fi
 done
