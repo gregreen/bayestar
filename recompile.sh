2e59c93ed3e911f47558e98d6a2ce58d244a80fa
Gregory Green <gregorymgreen@gmail.com>
2018-09-20 21:15:47 -0400

    Add commandline arg to recompile.sh to control profiling genration/use.

--- recompile.sh
+++ recompile.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# Profiling options
+prof_input=${1:-0}
+prof_gen="FALSE"
+prof_use="FALSE"
+if [[ ${prof_input} -eq 1 ]]; then
+    prof_gen="TRUE"
+    prof_use="FALSE"
+elif [[ ${prof_input} -eq 2 ]]; then
+    prof_gen="FALSE"
+    prof_use="TRUE"
+fi
+
+echo "PROFILING_GEN=${prof_gen}"
+echo "PROFILING_USE=${prof_use}"
+
 # Recompile bayestar
 rm cmake_install.cmake
 rm CMakeCache.txt
@@ -26,8 +41,8 @@ cmake \
     -DBoost_NO_BOOST_CMAKE=BOOL:ON \
     -DADDITIONAL_LINK_DIRS=/n/sw/fasrcsw/apps/Comp/gcc/7.1.0-fasrc01/boost/1.63.0-fasrc01/lib \
     -DCMAKE_BUILD_TYPE=RELEASE \
-    -DPROFILING_GEN=FALSE \
-    -DPROFILING_USE=FALSE \
+    -DPROFILING_GEN=${prof_gen} \
+    -DPROFILING_USE=${prof_use} \
     .
 
 make VERBOSE=1 -j
