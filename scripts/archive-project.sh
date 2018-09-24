0938cfcf8d8f2d2b18330a75dadc28d66eedb1c4
Gregory Green <gregorymgreen@gmail.com>
2018-09-24 17:59:25 -0400

    Add ".tar" to archive filenames automatically.

--- scripts/archive-project.sh
+++ scripts/archive-project.sh
@@ -9,9 +9,9 @@
 # 
 
 export COMMIT_HASH=`git rev-parse HEAD`
-git archive --format=tar --output=$1 ${COMMIT_HASH}
+git archive --format=tar --output=${1}.tar ${COMMIT_HASH}
 echo ${COMMIT_HASH} > commit.txt
-tar --append --file=$1 commit.txt
-gzip -3 $1
+tar --append --file=${1} commit.txt
+gzip -3 ${1}.tar
 
-echo "Project archived to $1.gz"
+echo "Project archived to ${1}.tar.gz"
