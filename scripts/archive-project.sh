80f0032bd23e82edb1a8cd1972b947de28199c4a
Gregory Green <gregorymgreen@gmail.com>
2018-10-25 11:23:04 -0400

    Fixed problem in which git commit hash was not archived.

--- scripts/archive-project.sh
+++ scripts/archive-project.sh
@@ -11,7 +11,7 @@
 export COMMIT_HASH=`git rev-parse HEAD`
 git archive --format=tar --output=${1}.tar ${COMMIT_HASH}
 echo ${COMMIT_HASH} > commit.txt
-tar --append --file=${1} commit.txt
+tar --append --file=${1}.tar commit.txt
 gzip -3 ${1}.tar
 
 echo "Project archived to ${1}.tar.gz"
